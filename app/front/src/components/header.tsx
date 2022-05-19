17b1f6be58a2559f58f70c5ecf966071aafd98e4
timur.ptashchenko <timur.ptashchenko@isoftbet.com>
2022-05-19 23:58:40 +0300

    Small update

--- app/front/src/components/header.tsx
+++ app/front/src/components/header.tsx
@@ -1,42 +1,18 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import './header.scss';
 import {NavLink} from "react-router-dom";
-import {randNum, sleep} from "../utils/utils";
 
 function Header(props: any) {
-    const [titleAnimationText, setTitleAnimationText] = useState('');
-    const titleAnimationTextVariants = [
-        " blog",
-        " page",
-        " personal page",
-        " example project?",
-        " fan page ✨⭐",
-        "`s abomination",
-    ]
-
-    async function animateTitle() {
-        const textVariants = [...titleAnimationTextVariants];
-        textVariants.splice(textVariants.indexOf(titleAnimationText), 1);
-        let titleText = textVariants[randNum(textVariants.length, 0)];
-        for (let char of titleText) {
-            await sleep(randNum(250, 25));
-            setTitleAnimationText(prevState => prevState += char + '▒');
-            await sleep(randNum(250, 25));
-            setTitleAnimationText(prevState => prevState.replace(/▒/g, ''));
-        }
-    }
 
     useEffect(() => {
-        animateTitle();
+
     }, []);
 
     return (
         <header>
             <section className={'main-header-section'}>
                 <span className={'title'}>
-                    <h1 className={'title-header'}>@TimurAztec{titleAnimationText}</h1>
-                    <h6 className={'title-subheader'}>aka @PingLord</h6>
-                    <a href={'mailto:timotiaztec@gmail.com'}>timotiaztec@gmail.com</a>
+                    <h1 className={'title-header'}>Julia</h1>
                 </span>
                 <nav>
                     <ul className={'nav-list'}>
